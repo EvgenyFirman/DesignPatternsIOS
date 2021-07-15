93c157ceb7b027776dcb91c80ae7e7f9cabbf443
Eugene <Eugeniolio@yandex.com>
2021-07-15 19:28:05 +0300

    base project

---
+++ RWBlueLibrary/LibraryAPI.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryAPI.swift
+//  RWBlueLibrary
+//
+//  Created by Евгений Фирман on 15.07.2021.
+//  Copyright © 2021 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+
+final class libraryAPI {
+  static let shared = libraryAPI()
+  
+  private init(){
+    
+  }
+}
---
+++ RWBlueLibrary/ViewController.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+  @IBOutlet var tableView: UITableView!
+  @IBOutlet var undoBarButtonItem: UIBarButtonItem!
+  @IBOutlet var trashBarButtonItem: UIBarButtonItem!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+}
+
