4cf4d11a4dff2f9cc1832fbb1d33fac8d920728f
Eugene <Eugeniolio@yandex.com>
2021-07-16 18:09:36 +0300

    facade,MVC,singleton,decorator added

--- RWBlueLibrary/LibraryAPI.swift
+++ RWBlueLibrary/LibraryAPI.swift
@@ -11,7 +11,34 @@ import Foundation
 final class libraryAPI {
   static let shared = libraryAPI()
   
+  private let persistencyManager = PersistencyManager()
+  
+  private let httpClient = HTTPClient()
+  
+  private let isOnline = false
+  
+  func getAlbums() -> [Album] {
+    
+    return persistencyManager.getAlbums()
+    
+  }
+  
+  func addAlbum(_ album: Album, at index: Int) {
+    persistencyManager.addAlbum(album, at: index)
+    if isOnline {
+      httpClient.postRequest("/api/addAlbum", body: album.description)
+    }
+  }
+  
+  func deleteAlbum(at index: Int) {
+    persistencyManager.deleteAlbum(at: index)
+    if isOnline {
+      httpClient.postRequest("/api/deleteAlbum", body: "\(index)")
+    }
+  }
+  
   private init(){
     
   }
 }
+
